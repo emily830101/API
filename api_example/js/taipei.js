eeb4a5c571864d3c787df2ba9b703c0b9660813f
emily830101 <s603860725@gmail.com>
2023-01-03 10:21:30 +0800

    weather example

---
+++ api_example/js/taipei.js
@@ -0,0 +1,15 @@
+$(function () {
+  $.ajax({
+    url: "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-B07587C1-6DD3-4673-9CF4-3344E4B60995&format=JSON&locationName=%E8%87%BA%E5%8C%97%E5%B8%82",
+    method: "GET",
+    dataType: "JSON",
+    // data: post才會用
+
+    success: function (res) {
+      console.log(res);
+    },
+    error: function (err) {
+      console.log(err);
+    }
+  });
+});
